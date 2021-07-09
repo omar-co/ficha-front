c808f048d947dbe3a93e520224909506918b93de
omarcoronado <T3rr3m0t0->
2021-07-09 17:48:13 -0500

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+            <h4>VINCULACIÓN ENTRE LOS PROGRAMAS PRESUPUESTARIOS, EL ANEXO TRANSVERSAL DE CAMBIO CLIMÁTICO Y LA CONTRIBUCIÓN DETERMINADA A NIVEL NACIONAL</h4>
+            <hr className="red"/>
+            <div className="row">
+                <div className="col-md-3">
+                    <ul className="nav nav-tabs tabs-left">
+                        <li className="active"><a href="#identificacion" data-toggle="tab">IDENTIFICACIÓN DEL PROGRAMA PRESUPUESTARIO</a></li>
+                        <li><a href="#vinculacion" data-toggle="tab">VINCULACIÓN CON LA CONTRIBUCIÓN DETERMINADA A NIVEL NACIONAL</a></li>
+                        <li><a href="#aportacion" data-toggle="tab">APORTACIÓN AL PROGRAMA ESPECIAL DE CAMBIO CLIMÁTICO</a></li>
+                        <li><a href="#cuantificacion" data-toggle="tab">CUANTIFICACIÓN PRESUPUESTAL</a></li>
+                        <li><a href="#indicadores" data-toggle="tab">VINCULACIÓN A INDICADORES</a></li>
+                        <li><a href="#identificacion" data-toggle="tab">IDENTIFICACIÓN DE ÁREAS DE MEJORA EN ACTIVIDADES ACTUALES</a></li>
+                        <li><a href="#componentes" data-toggle="tab">COMPONENTES DE MITIGACIÓN Y ADAPTACIÓN</a></li>
+                    </ul>
+                </div>
+                <div className="col-md-9">
+
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
