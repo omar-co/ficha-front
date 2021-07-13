0136b36669466a48a44fabe820d6773e6e95a4ac
omarcoronado <T3rr3m0t0->
2021-07-12 20:08:14 -0500

    programas presupuestales

--- src/components/Identification.tsx
+++ src/components/Identification.tsx
@@ -1,14 +1,51 @@
 import React from "react";
 import {useForm, SubmitHandler} from "react-hook-form";
 import {Ramos} from "../data/identification/Ramos";
-import {Modalidad} from "../data/identification/Modalidad";
-import {ProgramasPresupestales} from "../data/identification/ProgramasPresupestales";
+import {ProgramasPresupestales, Programas, Nombres} from "../data/identification/ProgramasPresupestales";
 
 function Identification({onSubmit}: {
     onSubmit: SubmitHandler<any>;
 }) {
 
-    const {handleSubmit, register} = useForm();
+    const {handleSubmit, register, getValues, setValue} = useForm();
+
+    const modalities = () => (
+        Ramos.filter(({value}) =>
+            value === getValues('ramo')).map(ramo =>
+            ramo.modalidad!.map(modalidad => (
+            <option value={modalidad}>{modalidad}</option>)))
+    );
+
+    const programa = () => (
+      Nombres.filter(({ramo, modalidad})=>
+          (ramo === getValues('ramo') && modalidad === getValues('modalidad'))).map(({value}) => (
+          <option value={value}>{value}</option>
+      ))
+    );
+
+    const programName = () => {
+        const ramo = Nombres.find(({ramo, modalidad, value}) => (
+            (ramo === getValues('ramo') && modalidad === getValues('modalidad') && value === getValues('programa'))
+        ));
+
+        if (ramo){
+            setValue('nombrePrograma', ramo.nombre);
+            return ramo.nombre;
+        }
+        return ''
+    };
+
+    const responsible = () => {
+        const ramo = Nombres.find(({ramo, modalidad, value}) => (
+            (ramo === getValues('ramo') && modalidad === getValues('modalidad') && value === getValues('programa'))
+        ));
+
+        if (ramo) {
+            return ramo.ur.map((value, index) => (
+                <option value={value}>{value}</option>
+            ))
+        }
+    }
 
     return (
         <div className="tab-pane active" id="identificacion">
@@ -16,7 +53,7 @@ function Identification({onSubmit}: {
                 <form onChange={handleSubmit(onSubmit)}>
                     <div className="form-group">
                         <label className='control-label' htmlFor="ramo">Ramo:</label>
-                        <select className='form-control' {...register("ramo")}>
+                        <select className='form-control' {...register("ramo", {valueAsNumber: true})}>
                             {Ramos.map(({value, label}, index) => <option value={value}>{label}</option>)}
                         </select>
                     </div>
@@ -24,26 +61,30 @@ function Identification({onSubmit}: {
                         <div className="form-group">
                             <label htmlFor="modalidad" className="control-label">Modalidad:</label>
                             <select className="form-control" {...register('modalidad')}>
-                                {Modalidad.map((index) => <option value={index}>{index}</option>)}
+                                <option value="">Selecciona una Opcion</option>
+                                {modalities()}
                             </select>
                             <div className="form-group">
                                 <label htmlFor="modalidad" className="control-label">
                                     ID_Programa presupuestario:
                                 </label>
-                                <select className="form-control" {...register('programa')}>
-                                    {ProgramasPresupestales.map((index) => <option value={index}>{index}</option>)}
+                                <select className="form-control" {...register('programa', {valueAsNumber:true})}>
+                                    <option value="">Selecciona una Opcion</option>
+                                    {programa()}
                                 </select>
                             </div>
                         </div>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="nombrePrograma" className="control-label">Nombre del programa presupuestario:</label>
-                        <input className="form-control" {...register('nombrePrograma')} disabled />
+                        <label htmlFor="nombrePrograma" className="control-label">Nombre del programa
+                            presupuestario:</label>
+                        <input className="form-control" {...register('nombrePrograma')} value={programName()} readOnly/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="unidadResponsable" className="control-label">Unidad Responsable:</label>
                         <select className="form-control" {...register('unidadResponsable')} >
-                            <option value="">demo</option>
+                            <option value="">Selecciona una Opcion</option>
+                            {responsible()}
                         </select>
                     </div>
                     <div className="form-group">
@@ -51,11 +92,13 @@ function Identification({onSubmit}: {
                         <input className="form-control" {...register('objetivo')} />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="componentes" className="control-label">Bienes y productos generados con posible incidencia (Componentes)_1:</label>
+                        <label htmlFor="componentes" className="control-label">Bienes y productos generados con posible
+                            incidencia (Componentes)_1:</label>
                         <input className="form-control" {...register('componentes')} />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="actividades" className="control-label">Actividad(es) actual(es) con posible incidencia_1:</label>
+                        <label htmlFor="actividades" className="control-label">Actividad(es) actual(es) con posible
+                            incidencia_1:</label>
                         <textarea className="form-control" {...register('actividades')} />
                     </div>
                 </form>
