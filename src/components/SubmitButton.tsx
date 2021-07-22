c2b4c9e5ee54630236124af28b4b4d981c593003
Francisco Guadarrama <frankguadarrama@outlook.com>
2021-07-22 17:40:54 -0500

    axios without baseUrl

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import axios from "axios";
 
 function SubmitButton({store}: {
     store: any;
 }) {
 
-    const logger = () => console.log(store);
+
+     async function saveData(store){
+         try {
+             return await axios({
+                url: `url/api/`,
+                method: 'POST',
+                data: store,
+            });
+        }catch(e){
+            console.log(e);
+        }
+    }
+
+    //const logger = () => console.log(store);
 
     return (
         <div className="form-group">
-            <button className='btn btn-primary' onClick={logger} disabled >Descargar Excel</button>
+            <button className='btn btn-primary' onClick={() => saveData(store)}  >Enviar</button>
         </div>
     );
 
