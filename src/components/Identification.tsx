0cfa6a5da9ae113bef2b3972d538ac84f1b12f1d
Francisco Guadarrama <frankguadarrama@outlook.com>
2021-07-26 18:02:12 -0500

    missing fields in identification

--- src/components/Identification.tsx
+++ src/components/Identification.tsx
@@ -3,14 +3,18 @@ import {useForm, SubmitHandler} from "react-hook-form";
 import {Ramos} from "../data/identification/Ramos";
 import {Nombres} from "../data/identification/ProgramasPresupestales";
 import {Programas} from "../data/identification/Modalidad";
-import axios from "axios";
 import {Finalidad, Funcion, Subfuncion} from "../data/cuantificacion/Presupuestos";
+import axios from "axios";
 
 function Identification({onSubmit}: {
     onSubmit: SubmitHandler<any>;
 }) {
 
-    const {handleSubmit, register, getValues, formState: { errors }} = useForm({mode: "onBlur"});
+    const initial: any[] = [];
+    const {handleSubmit, register, getValues, formState: {errors}} = useForm({mode: "onBlur"});
+    const [gasto, setGasto] = useState(initial);
+    const [entidad, setEntidad] = useState(initial);
+    const [fuente, setFuente] = useState(initial);
 
     const modalities = () => (
         getValues('ramo') ? Ramos.filter(({value}) =>
@@ -87,6 +91,50 @@ function Identification({onSubmit}: {
         ) : null
     );
 
+    const fetchData = () => {
+        const gastoApi = process.env.REACT_APP_API_URL + '/tipo-gasto';
+        const entidadApi = process.env.REACT_APP_API_URL + '/entidad-federativa';
+        const fuenteApi = process.env.REACT_APP_API_URL + '/fuente-financiamiento';
+
+        const getGasto = axios.get(gastoApi)
+        const getEntidad = axios.get(entidadApi)
+        const getFuente = axios.get(fuenteApi)
+        axios.all([getGasto, getEntidad, getFuente]).then(
+            axios.spread((...allData) => {
+                const allGastoData = allData[0].data;
+                const allEntidadData = allData[1].data;
+                const allFuenteData = allData[2].data;
+
+                setGasto(allGastoData);
+                setEntidad(allEntidadData);
+                setFuente(allFuenteData);
+            })
+        )
+    }
+
+    const spendingType = () => (
+        gasto.map((obj) =>
+            <option value={obj.id}>{obj.nombre}</option>
+        )
+    );
+
+    const entity = () => (
+        entidad.map((obj) =>
+            <option value={obj.id}>{obj.nombre}</option>
+        )
+    );
+
+    const sources = () => (
+        fuente.map((obj) =>
+            <option value={obj.id}>{obj.nombre}</option>
+        )
+    );
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+
     return (
         <div className="tab-pane active" id="identificacion">
             <div className="panel-body">
@@ -94,8 +142,12 @@ function Identification({onSubmit}: {
                     <div className="row">
                         <div className="form-group col-md-12">
                             <label className='control-label' htmlFor="ramo">Ramo:</label>
-                            <select className='form-control' {...register("ramo", {valueAsNumber: true, required: true})}>
-                                {Ramos.map(({value, label}, index) => <option key={index} value={value}>{label}</option>)}
+                            <select className='form-control' {...register("ramo", {
+                                valueAsNumber: true,
+                                required: true
+                            })}>
+                                {Ramos.map(({value, label}, index) => <option key={index}
+                                                                              value={value}>{label}</option>)}
                             </select>
                             {errors.ramo?.type === 'required' && <span className="obligatorio">Dato Obligatorio</span>}
                         </div>
@@ -107,7 +159,8 @@ function Identification({onSubmit}: {
                                 <option value="">Selecciona una Opcion</option>
                                 {modalities()}
                             </select>
-                            {errors.modalidad?.type === 'required' && <span className="obligatorio">Dato Obligatorio</span>}
+                            {errors.modalidad?.type === 'required' &&
+                            <span className="obligatorio">Dato Obligatorio</span>}
                         </div>
                         <div className="form-group col-md-6">
                             <label htmlFor="modalidad" className="control-label">
@@ -179,6 +232,43 @@ function Identification({onSubmit}: {
                                 <option>Seleccione una opción</option>
                                 {subfunctions()}
                             </select>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <label htmlFor="objetoGasto" className="control-label">Objeto del gasto:</label>
+                            <input className="form-control" {...register('objetoGasto')}/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group col-md-6">
+                            <label htmlFor="tipoGasto" className="control-label">Tipo de gasto:</label>
+                            <select className="form-control" {...register('tipoGasto')}>
+                                <option>Seleccione una opción</option>
+                                {spendingType()}
+                            </select>
+                        </div>
+                        <div className="col-md-6">
+                            <label htmlFor="fuenteFinanciamiento" className="control-label">Fuente de
+                                Financiamiento:</label>
+                            <select className='form-control' {...register('fuenteFinanciamiento')}>
+                                <option>Seleccione una opción</option>
+                                {sources()}
+                            </select>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <label htmlFor="entidadFederativa" className="control-label">Entidad Federativa:</label>
+                            <select className='form-control' {...register('entidadFederativa')}>
+                                <option>Seleccione una opción</option>
+                                {entity()}
+                            </select>
+                        </div>
+                        <div className="col-md-6">
+                            <label htmlFor="claveCartera" className="control-label">Clave de Cartera:</label>
+                            <input className='form-control' {...register('claveCartera')}/>
                         </div>
                     </div>
                 </form>
