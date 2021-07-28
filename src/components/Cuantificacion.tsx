6bc0237e4a1a932aebcf0e925f2e49d5d2827c63
omarcoronado <T3rr3m0t0->
2021-07-28 00:08:09 -0500

    validacion y pregunta

--- src/components/Cuantificacion.tsx
+++ src/components/Cuantificacion.tsx
@@ -1,7 +1,5 @@
-import React, { useState} from "react";
-import {SubmitHandler, useForm} from "react-hook-form";
-import CurrencyInput from 'react-currency-input-field';
-import {Marcadores} from "../data/shared";
+import React from "react";
+import {SubmitHandler} from "react-hook-form";
 
 import FiltroModal from "./FiltroModal";
 
@@ -10,35 +8,6 @@ function Cuantificacion({onSubmit, store}: {
     store: any;
     onSubmit: SubmitHandler<any>;
 }) {
-
-    const {handleSubmit, register, setValue} = useForm();
-    const [pp, setPp] = useState<string | undefined>(' ');
-    const [rawPp, setRawPp] = useState<string | undefined>(' ');
-   // const [data, setData] = useState({data: []});
-
-    const formatAndRawPpValue = (pp: string | undefined): void => {
-        setRawPp(pp === undefined ? 'undefined' : pp || ' ');
-        setPp(pp);
-        if (typeof rawPp === "string") {
-            store.rawPp = parseInt(rawPp);
-        }
-        setValue('atcc', (store.rawPp * Marcadores[store.marcador]));
-    };
-    /*
-      useEffect(() => {
-          axios.get(process.env.REACT_APP_API_URL + '/capitulo').then(
-              res => setData(res.data)
-          );
-      });
-
-      /*
-      const capitulos = () => (
-          data.data && getValues('capitulo') ? data.data.filter(({capitulo}) =>
-              capitulo === getValues('capitulo')).map(concepto => (
-                  <option value={concepto}>{concepto}</option>
-              )
-          ) : null
-      ); */
 
     return (
         <div className="tab-pane" id="cuantificacion">
