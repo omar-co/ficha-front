5a9638e97757c962c9cfa6596b420d3fd814292a
Francisco Guadarrama <frankguadarrama@outlook.com>
2021-08-06 00:55:23 -0500

    bug fixed

--- src/components/FiltroModal.tsx
+++ src/components/FiltroModal.tsx
@@ -9,22 +9,20 @@ function FiltroModal( {store}: {
     const initial: any[] = [];
     const [data, setData] = useState(initial);
 
-    const getFilterData = () => {
-        if(store.ramo && store.programa && store.modalidad){
-            const url = '/relacion-economica/' + store.ramo + '/' + store.programa + '/' + (!store.directamente) + '/' + store.modalidad;
-            axios.get(process.env.REACT_APP_API_URL + url).then(
-                (response) => {
-                    setData(response.data)
-                }
-            )
-        }
-    }
 
     useEffect(() => {
+        const getFilterData = () => {
+            if(store.ramo && store.programa && store.modalidad){
+                const url = '/relacion-economica/' + store.ramo + '/' + store.programa + '/' + (!store.directamente) + '/' + store.modalidad;
+                axios.get(process.env.REACT_APP_API_URL + url).then(
+                    (response) => {
+                        setData(response.data)
+                    }
+                )
+            }
+        };
         getFilterData();
-    });
-
-
+    }, [store.ramo, store.programa, store.directamente, store.modalidad]);
 
 
     return(
