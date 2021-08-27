531c6f158b6a81345a608957cefdbe73098763a7
Francisco Guadarrama <frankguadarrama@outlook.com>
2021-08-27 00:38:52 -0500

    delete button

--- src/components/Cuantificacion.tsx
+++ src/components/Cuantificacion.tsx
@@ -3,12 +3,26 @@ import React from "react";
 
 import FiltroModal from "./FiltroModal";
 import TabsMenu from "./TabsMenu";
+import {useForm} from "react-hook-form";
+import axios from "axios";
+import {authHeader} from "../helpers/AuthHeader";
 
 
 function Cuantificacion({store}: {
     store: any;
     setStore: any
 }) {
+
+    const { getValues, register } = useForm();
+
+    const deletePp = () => {
+        let id = getValues('delete');
+        if (window.confirm("Quieres borrar el presupuesto?")) {
+            axios.delete(process.env.REACT_APP_API_URL + '/pp/delete/' + id, {headers: authHeader()}).then(
+                () => true
+            )
+        }
+    }
 
     return (
         <>
@@ -22,13 +36,23 @@ function Cuantificacion({store}: {
                 <div className="col-md-9">
                     <div className="tab-pane" id="cuantificacion">
                         <div className="panel-body">
-                            <label className="control-label">Cuantificación del presupuesto</label>
-                            <hr className="red"/>
-
-                            <FiltroModal store={store}/>
-
-                            <br/>
-                            <br/>
+                            <div className="row">
+                                <label className="control-label">Cuantificación del presupuesto</label>
+                                <hr className="red"/>
+                            </div>
+                            <div className="row">
+                                <div className="pull-right col-md-1">
+                                    <button type="button" className="btn btn-sm btn-primary" onClick={deletePp}>Eliminar Pp</button>
+                                </div>
+                                <div className="pull-right">
+                                    <input type="text" className="form-control" {...register('delete')} placeholder="Eliminar Pp"/>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <FiltroModal store={store}/>
+                                <br/>
+                                <br/>
+                            </div>
                         </div>
                     </div>
                 </div>
