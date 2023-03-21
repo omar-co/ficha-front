7896c4d0f7fe9764908ed107350b0b39f3992ba2
Omar Coronado <omar.goco@gmail.com>
2023-03-21 23:06:20 +0000

    añade formulario para crear politica publica

---
+++ src/components/admin/politicasPublicas/PoliticasPublicasCreate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PoliticasPublicasForm from "./PoliticasPublicasForm";
+
+const PoliticasPublicasCreate = () => {
+
+    return (
+        <div>
+            <PoliticasPublicasForm></PoliticasPublicasForm>
+        </div>
+    )
+}
+
+export default PoliticasPublicasCreate;
