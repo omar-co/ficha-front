cfb75b45e02c6f2e180481875f14414db9779bce
Omar Coronado <omar.goco@gmail.com>
2023-04-05 20:34:02 +0100

    politicas publicas en progreso

---
+++ src/components/admin/politicasPublicas/PoliticasPublicasValores.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function PoliticasPublicasValores({values}: {
+    values: any;
+}) {
+
+    return (
+        <>
+            <div className="col-md-6">
+                <h3>Valores</h3>
+                <button className="btn btn-primary ">Agregar Valor</button>
+            </div>
+        </>
+    )
+}
+
+export default PoliticasPublicasValores;
