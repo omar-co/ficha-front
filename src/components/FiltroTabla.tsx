e0352aeb5efce600e22071165d0ef3ea0d6774f7
Omar Coronado <omar.goco@gmail.com>
2024-05-13 15:15:40 +0100

    agrega log para debug

--- src/components/FiltroTabla.tsx
+++ src/components/FiltroTabla.tsx
@@ -59,8 +59,9 @@ export default function FiltroTabla({store, mainStore}: {
     ];
 
     const validarModaldiad = (item) => {
-        console.log(item)
         setChanged([...changed, item]);
+        console.log(item)
+        console.log(changed)
     };
 
     return (
